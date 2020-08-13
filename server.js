7b9d69eb7d6749fb7d86e11e1cbeb2bdafa4319c
Joshua Lebert <expertjosh2000@gmail.com>
2020-08-13 16:59:25 -0400

    setup host (listen and useStatic) and routes for main folders

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 4000;
-// req.get('host');
-console.log(port);
+const port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}.`);
 });
 
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.sendFile(path.join(__dirname, 'public'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/projects'));
+});
+
+app.get('/tips', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/tips'));
+});
+
+app.get('/resources', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/resources'));
 });
